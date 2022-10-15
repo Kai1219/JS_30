c420111c4932227009e4a366652f9355c0f97f85
Kai <jen147774ny@gmail.com>
2022-10-15 13:58:57 +0800

    overView

---
+++ Overview/script.js
@@ -0,0 +1,267 @@
+const cards = [
+    {
+        name: 'Day01',
+        title: 'JavaScript Drum Kit',
+        workLink: 'D01/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10291833',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D01',
+        imgUrl: '/Overview/img/D01.PNG',
+    },
+    {
+        name: 'Day02',
+        title: 'CSS + JS Clock',
+        workLink: 'D02/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10292502',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D02',
+        imgUrl: '/Overview/img/D02.PNG',
+    },
+    {
+        name: 'Day03',
+        title: 'Playing with CSS Variables and JS',
+        workLink: 'D03/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10292536',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D03',
+        imgUrl: '/Overview/img/D03.PNG',
+    },
+    {
+        name: 'Day04',
+        title: 'Array Cardio Day 1',
+        workLink: 'D04/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10293222',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D04',
+        imgUrl: '/Overview/img/D04.PNG',
+    },
+    {
+        name: 'Day05',
+        title: 'Flex Panels Image Gallery',
+        workLink: 'D05/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10293239',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D05',
+        imgUrl: '/Overview/img/D05.PNG',
+    },
+    {
+        name: 'Day06',
+        title: 'Ajax Type Ahead',
+        workLink: 'D06/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10294504',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D06',
+        imgUrl: '/Overview/img/D06.PNG',
+    },
+    {
+        name: 'Day07',
+        title: 'Array Cardio Day 2',
+        workLink: 'D07/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10295046',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D07',
+        imgUrl: '/Overview/img/D07.PNG',
+    },
+    {
+        name: 'Day08',
+        title: 'Fun with HTML5 Canvas',
+        workLink: 'D08/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10295760',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D08',
+        imgUrl: '/Overview/img/D08.PNG',
+    },
+    {
+        name: 'Day09',
+        title: '14 Must Know Dev Tools Tricks',
+        workLink: 'D09/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10296528',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D09',
+        imgUrl: '/Overview/img/D09.PNG',
+    },
+    {
+        name: 'Day10',
+        title: 'Hold Shift to Check Multiple Checkboxes',
+        workLink: 'D10/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10297275',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D10',
+        imgUrl: '/Overview/img/D10.PNG',
+    },
+    {
+        name: 'Day11',
+        title: 'Custom HTML5 Video Player',
+        workLink: 'D11/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10298143',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D11',
+        imgUrl: '/Overview/img/D11.PNG',
+    },
+    {
+        name: 'Day12',
+        title: 'Key Sequence Detection (KONAMI CODE)',
+        workLink: 'D12/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10298947',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D12',
+        imgUrl: '/Overview/img/D12.PNG',
+    },
+    {
+        name: 'Day13',
+        title: 'Slide In on Scroll',
+        workLink: 'D13/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10299832',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D13',
+        imgUrl: '/Overview/img/D13.PNG',
+    },
+    {
+        name: 'Day14',
+        title: 'Object and Arrays - Reference VS Copy',
+        workLink: 'D14/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10300497',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D14',
+        imgUrl: '/Overview/img/D14.PNG',
+    },
+    {
+        name: 'Day15',
+        title: 'LocalStorage and Event Delegation',
+        workLink: 'D15/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10301087',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D15',
+        imgUrl: '/Overview/img/D15.PNG',
+    },
+    {
+        name: 'Day16',
+        title: 'CSS Text Shadow Mouse Move Effect',
+        workLink: 'D16/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10301785',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D16',
+        imgUrl: '/Overview/img/D16.PNG',
+    },
+    {
+        name: 'Day17',
+        title: 'Sorting Band Names without articles',
+        workLink: 'D17/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10302098',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D17',
+        imgUrl: '/Overview/img/D17.PNG',
+    },
+    {
+        name: 'Day18',
+        title: 'Tally String Times with Reduce',
+        workLink: 'D18/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10302679',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D18',
+        imgUrl: '/Overview/img/D18.PNG',
+    },
+    {
+        name: 'Day19',
+        title: 'Unreal Webcam Fun',
+        workLink: 'D19/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10302996',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D19',
+        imgUrl: '/Overview/img/D19.PNG',
+    },
+    {
+        name: 'Day20',
+        title: 'Native Speech Recognition',
+        workLink: 'D20/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10304156',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D20',
+        imgUrl: '/Overview/img/D20.PNG',
+    },
+    {
+        name: 'Day21',
+        title: 'Geolocation based Speedometer and Compass',
+        workLink: 'D21/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10304573',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D21',
+        imgUrl: '/Overview/img/D21.PNG',
+    },
+    {
+        name: 'Day22',
+        title: 'Follow Along Links',
+        workLink: 'D22/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10305112',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D22',
+        imgUrl: '/Overview/img/D22.PNG',
+    },
+    {
+        name: 'Day23',
+        title: 'Speech Synthesis',
+        workLink: 'D23/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10305630',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D23',
+        imgUrl: '/Overview/img/D23.PNG',
+    },
+    {
+        name: 'Day24',
+        title: 'Sticky Nav',
+        workLink: 'D24/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10305809',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D24',
+        imgUrl: '/Overview/img/D24.PNG',
+    },
+    {
+        name: 'Day25',
+        title: 'Event Capture, Propagation, Bubbling and Once',
+        workLink: 'D25/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10306496',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D25',
+        imgUrl: '/Overview/img/D25.PNG',
+    },
+    {
+        name: 'Day26',
+        title: 'Stripe Follow Along Dropdown',
+        workLink: 'D26/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10306843',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D26',
+        imgUrl: '/Overview/img/D26.PNG',
+    },
+    {
+        name: 'Day27',
+        title: 'Click and Drag to Scroll',
+        workLink: 'D27/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10307473',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D27',
+        imgUrl: '/Overview/img/D27.PNG',
+    },
+    {
+        name: 'Day28',
+        title: 'Video Speed Controller UI',
+        workLink: 'D28/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10307871',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D28',
+        imgUrl: '/Overview/img/D28.PNG',
+    },
+    {
+        name: 'Day29',
+        title: 'Countdown Clock',
+        workLink: 'D29/index.html',
+        noteLink: 'https://ithelp.ithome.com.tw/articles/10308226',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D29',
+        imgUrl: '/Overview/img/D29.PNG',
+    },
+    {
+        name: 'Day30',
+        title: 'Whack A Mole Game',
+        workLink: 'D30/index.html',
+        noteLink: '',
+        gitLink: 'https://github.com/Kai1219/JS_30/tree/gh-pages/D30',
+        imgUrl: '/Overview/img/D30.PNG',
+    },
+];
+const container = document.querySelector('.day-ol');
+
+const display = cards
+    .map(
+        (card) =>
+            `<li class="day-li">
+        <a class="day-link" href="${card.workLink}" target="_blank">
+            <img src="${card.imgUrl}" alt="" />
+            <span>查看效果>></span>
+        </a>
+        <div class="text">
+            <div class="title">
+                <h2>${card.name}</h2>
+                <h3>${card.title}</h3>
+            </div>
+            <div class="links">
+                <a href="${card.noteLink}" target="_blank"></a>
+                <a href="${card.gitLink}" target="_blank"></a>
+            </div>
+        </div>
+    </li>`
+    )
+    .join('');
+
+container.innerHTML = display;
